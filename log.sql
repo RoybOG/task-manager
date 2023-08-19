8604242c07ef1c3dadf7c41366d0a5bb7baee8ab
Itai <itaibarak12345@gmail.com>
2023-08-19 17:29:03 +0300

    Python environment and server2

---
+++ log.sql
@@ -0,0 +1,10 @@
+use task_manager_db;
+-- insert into tasks values('sci5n7yE0', 'itayb',2,'python exercise\nfinish a website');
+-- create table g(a varchar(255),b varchar(255));
+
+SELECT
+    *
+FROM
+ tasks;
+ --    mysql.general_log
+-- where event_time > DATE_ADD(CURRENT_TIMESTAMP(), INTERVAL -1 MINUTE);
